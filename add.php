2f3b85a65c0d51b0a05dbeba2cbf6e2768e0bfcf
bhoomibalegar-sys <bhoomibalegar@gmail.com>
2026-05-11 14:37:52 +0530

    Update add.php

--- add.php
+++ add.php
@@ -2,20 +2,34 @@
 
 $conn = new mysqli("127.0.0.1", "root", "", "vehicle_management");
 
-$data = json_decode(file_get_contents("php://input"));
+$uploadDir = "uploads/";
 
-if (
-    empty($data->vehicle_number) ||
-    empty($data->fc) ||
-    empty($data->insurance) ||
-    empty($data->emission)
-) {
-    echo "Invalid input";
-    exit;
+// FUNCTION TO SAVE FILE
+function saveFile($file, $uploadDir) {
+    if ($file['name'] == "") return "";
+
+    $filename = time() . "_" . basename($file['name']);
+    $target = $uploadDir . $filename;
+
+    move_uploaded_file($file['tmp_name'], $target);
+
+    return $filename;
 }
 
+// SAVE FILES
+$fc_file = saveFile($_FILES['fc_file'], $uploadDir);
+$insurance_file = saveFile($_FILES['insurance_file'], $uploadDir);
+$emission_file = saveFile($_FILES['emission_file'], $uploadDir);
+$ap_file = saveFile($_FILES['ap_file'], $uploadDir);
+$tn_file = saveFile($_FILES['tn_file'], $uploadDir);
+$kl_file = saveFile($_FILES['kl_file'], $uploadDir);
+
+// INSERT DATA
 $conn->query("INSERT INTO vehicles 
-(vehicle_number, fc_date, insurance_date, emission_date, ap_tp, tn_tp, kl_tp)
+(vehicle_number, fc_date, insurance_date, emission_date, ap_tp, tn_tp, kl_tp,
+fc_file, insurance_file, emission_file, ap_file, tn_file, kl_file)
 VALUES 
-('$data->vehicle_number','$data->fc','$data->insurance','$data->emission','$data->ap','$data->tn','$data->kl')");
+('{$_POST['vehicle_number']}','{$_POST['fc']}','{$_POST['insurance']}','{$_POST['emission']}',
+'{$_POST['ap']}','{$_POST['tn']}','{$_POST['kl']}',
+'$fc_file','$insurance_file','$emission_file','$ap_file','$tn_file','$kl_file')");
 ?>
