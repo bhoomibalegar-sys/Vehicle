89fe1a104c1ea9e7443ca40c12df15bbc6eb8d8d
bhoomibalegar-sys <bhoomibalegar@gmail.com>
2026-05-11 13:15:51 +0530

    Update add.php

--- add.php
+++ add.php
@@ -1,7 +1,18 @@
 <?php
+
 $conn = new mysqli("127.0.0.1", "root", "", "vehicle_management");
 
 $data = json_decode(file_get_contents("php://input"));
+
+if (
+    empty($data->vehicle_number) ||
+    empty($data->fc) ||
+    empty($data->insurance) ||
+    empty($data->emission)
+) {
+    echo "Invalid input";
+    exit;
+}
 
 $conn->query("INSERT INTO vehicles 
 (vehicle_number, fc_date, insurance_date, emission_date, ap_tp, tn_tp, kl_tp)
